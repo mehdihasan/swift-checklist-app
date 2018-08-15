50264bebcadb298fa20cb63413bb43d821ea551e
Mehdi <hasanmehdi.2009@gmail.com>
2018-08-15 12:57:46 +0600

    done button disabled if text is empty

--- MyChekListApp/AdditemTableViewController.swift
+++ MyChekListApp/AdditemTableViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class AdditemTableViewController: UITableViewController {
+class AdditemTableViewController: UITableViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        textField.becomeFirstResponder()
+        doneButton.isEnabled = false
     }
 
     @IBAction func cancel() {
@@ -21,5 +30,31 @@ class AdditemTableViewController: UITableViewController {
     
     @IBAction func done() {
         navigationController?.popViewController(animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)
+        let newText = oldText.replacingCharacters(in: stringRange!, with: string)
+        
+        print("NEW TEXT \(newText)")
+        
+        if newText.isEmpty {
+            doneButton.isEnabled = false
+        } else {
+            doneButton.isEnabled = true
+        }
+        
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneButton.isEnabled = false
+        return true
     }
 }
